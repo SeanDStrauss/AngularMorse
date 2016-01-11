319cee0a48680d7234ac49f35e51c3f673dcf181
Strauss <you@example.com>
2016-01-10 22:26:29 -0500

    fixed minor bugs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,7 +11,7 @@ angular.module('morseCodeApp')
   .controller('MainCtrl', function () {
     var vm = this;
 
-    var morse = {
+     vm.morse = {
         'a':'.-', 'b':'-...' , 'c':'-.-.' ,'d':'-..', 'e':'.',
         'f': '..-.', 'g':'--.', 'h': '....', 'i': '..', 'j': '.---',
         'k':'-.-', 'l':'.-..', 'm':'--', 'n':'-.', 'o':'---',
@@ -27,11 +27,12 @@ angular.module('morseCodeApp')
 
 
     vm.morseEncoder = function (text) {
+      text = text.toLowerCase();
       var translatedArray = [];
       for(var i = 0; i < text.length; i++) {
-        for(var key in morse) {
+        for(var key in vm.morse) {
           if (key == text[i]) {
-            var translated = morse[key] + " | ";
+            var translated = vm.morse[key] + " | ";
             translatedArray.push(translated);
           }
         }
