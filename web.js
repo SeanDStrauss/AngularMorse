b7aa8c6daadfd3c4e55c3fb89c8bda3ebe5f02d0
Strauss <you@example.com>
2016-01-10 22:07:09 -0500

    heroku ready

---
+++ web.js
@@ -0,0 +1,11 @@
+/**
+ * Created by kwr778 on 1/10/16.
+ */
+var gzippo = require('gzippo');
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
